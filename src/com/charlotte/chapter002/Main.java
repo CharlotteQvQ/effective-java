a5d91244aaf74e0848251b6494c5c9f690ad568e
Charlotte Li <charlotte_ljh@163.com>
2019-07-30 16:23:10 +0800

    update chapter002

---
+++ src/com/charlotte/chapter002/Main.java
@@ -0,0 +1,21 @@
+package com.charlotte.chapter002;
+
+/**
+ * @Author:Charlotte Li
+ * @Date:2019-07-30 15:24
+ * @Description:当构造方法参数过多时使用 builder 模式
+ * @version:1.0
+ */
+public class Main {
+    public static void main(String[] args){
+        /*当设计类的构造方法或静态工厂的参数超过几个时，Builder 模式是一个不错的选择，
+        特别是如果许 多参数是可选的或相同类型的。客户端代码比使用伸缩构造方法(telescoping constructors)更容易读写，
+        并且 builder 比 JavaBeans 更安全。*/
+        Production production = new Production.Builder("Car",999999).type("SAIC").build();
+        System.out.println(production.toString());
+
+        /*Builder 模式非常适合类层次结构。
+        使用平行层次的 builder，每个嵌套在相应的类中。
+        抽象类有抽象的 builder;具体的类有具体的 builder。*/
+    }
+}
---
+++ src/com/charlotte/chapter002/Production.java
@@ -0,0 +1,84 @@
+package com.charlotte.chapter002;
+
+import java.util.Date;
+
+/**
+ * @Author:Charlotte Li
+ * @Date:2019-07-30 15:26
+ * @Description:
+ * @version:1.0
+ */
+public class Production {
+
+    //名字--required
+    private final String name;
+
+    //价格--required
+    private final int price;
+
+    //类型--not required
+    private final String type;
+
+    //产地--not required
+    private final String originPlace;
+
+    //用途--not required
+    private final String usage;
+
+    //生产日期--not required
+    private final Date productionTime;
+
+    public static class Builder{
+        private final String name;
+        private final int price;
+        private String type = "";
+        private String originPlace = "";
+        private String usage = "";
+        private Date productionTime = null;
+
+        public Builder(String name,int price){
+            this.name = name;
+            this.price = price;
+        }
+
+        public Builder type(String type){
+            this.type = type;
+            return this;
+        }
+
+        public Builder originPlace(String originPlace){
+            this.originPlace = originPlace;
+            return this;
+        }
+
+        public Builder usage(String usage){
+            this.usage = usage;
+            return this;
+        }
+
+        public Builder productionTime(Date productionTime){
+            this.productionTime = productionTime;
+            return this;
+        }
+
+        public Production build(){
+            return new Production(this);
+        }
+    }
+
+    public Production(Builder builder){
+        this.name = builder.name;
+        this.price = builder.price;
+        this.type = builder.type;
+        this.originPlace = builder.originPlace;
+        this.usage = builder.usage;
+        this.productionTime = builder.productionTime;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Production:[name:%s,price:%d,type:%s,originPlace:%s,usage:%s,productionTime:%s]",
+                name,price,type,originPlace,usage,productionTime==null?"":productionTime.toString());
+    }
+
+}
