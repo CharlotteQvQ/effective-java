cae93b9577f52d2ec9fe0bb075caa0f08eacdd31
Charlotte Li <charlotte_ljh@163.com>
2019-07-30 11:22:24 +0800

    update chapter001 and readme.md

---
+++ src/com/charlotte/chapter001/Main.java
@@ -0,0 +1,24 @@
+package com.charlotte.chapter001;
+
+/**
+ * @Author:Charlotte Li
+ * @Date:2019-07-30 10:59
+ * @Description:考虑用静态工厂模式代替构造器
+ * @version:1.0
+ */
+public class Main {
+    public static void main(String[] args){
+        //1.可读性强
+        RandomIntGenerator randomIntGenerator;
+
+        randomIntGenerator = RandomIntGenerator.between(10,100);
+        System.out.println(randomIntGenerator.toString());
+        randomIntGenerator = RandomIntGenerator.bigger(10);
+        System.out.println(randomIntGenerator.toString());
+        randomIntGenerator = RandomIntGenerator.smaller(100);
+        System.out.println(randomIntGenerator.toString());
+
+        //2.不需要每次都创建一个新对象
+
+    }
+}
---
+++ src/com/charlotte/chapter001/RandomIntGenerator.java
@@ -0,0 +1,40 @@
+package com.charlotte.chapter001;
+
+/**
+ * @Author:Charlotte Li
+ * @Date:2019-07-30 11:08
+ * @Description:返回随机整型数
+ * @version:1.0
+ */
+public class RandomIntGenerator {
+
+    private int min = Integer.MIN_VALUE;
+
+    private int max = Integer.MAX_VALUE;
+
+    //构造函数
+    private RandomIntGenerator(int min,int max){
+        this.min = min;
+        this.max = max;
+    }
+
+    //> min && < max
+    public static RandomIntGenerator between(int min,int max){
+        return new RandomIntGenerator(min, max);
+    }
+
+    //> min && < Integer.MAX_VALUE
+    public static RandomIntGenerator bigger(int min){
+        return new RandomIntGenerator(min,Integer.MAX_VALUE);
+    }
+
+    //> Integer.MIN_VALUE && < max
+    public static RandomIntGenerator smaller(int max){
+        return new RandomIntGenerator(Integer.MIN_VALUE,max);
+    }
+
+    @Override
+    public String toString(){
+        return String.format("The max value is %d, and the min value is %d.",this.max,this.min);
+    }
+}
